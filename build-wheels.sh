c538eb91ddccc230ddf2a71a3cee78ec8d500a7e
Dan Holdsworth <daniel.s.holdsworth@gmail.com>
2020-10-29 11:26:11 +0000

    Update build-wheels.sh
    updated with a script that works for my env

--- build-wheels.sh
+++ build-wheels.sh
@@ -1,26 +1,15 @@
-#!/usr/bin/env bash
-pythons="cp36-cp36m cp37-cp37m cp38-cp38"
+#!/bin/bash -x
 build_dir=${PWD}
-export PATH="/opt/go/bin:$PATH" HOME=/tmp
-
+export HOME=/tmp
 # Build source tar
-/opt/python/cp38-cp38/bin/python3 setup.py sdist
-
+python3 setup.py sdist
 # Wheels will be written to /tmp before being auditwheel-repaired
 cd /tmp
-for py in $pythons; do
-    pybin="/opt/python/${py}/bin"
-    "${pybin}/pip" install -r "${build_dir}/dev-requirements.txt"
-    "${pybin}/pip" wheel --no-deps --wheel-dir /tmp "${build_dir}"/dist/*.tar.gz
-done
+pip install -r "${build_dir}/dev-requirements.txt"
+pip wheel --no-deps --wheel-dir /tmp "${build_dir}"/dist/*.tar.gz
 ls ./*.whl | xargs -n1 --verbose auditwheel repair --wheel-dir "${build_dir}/dist"
-
 # Install packages and test
-for py in $pythons; do
-    pybin="/opt/python/${py}/bin"
-    "${pybin}/pip" install pygohcl --no-index -f "${build_dir}/dist"
-    "${pybin}/pytest" -p no:cacheprovider "${build_dir}/tests"
-done
-
+pip install pygohcl --no-index -f "${build_dir}/dist"
+pytest -p no:cacheprovider "${build_dir}/tests"
 # Auditwheel-repaired wheels
 ls -al "${build_dir}/dist"
