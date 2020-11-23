9d776942cf2474449aa8c5e6ffe453ffe6c5d626
Dan Holdsworth <daniel.s.holdsworth@gmail.com>
2020-11-23 16:48:16 +0000

    Update build-wheels.sh

--- build-wheels.sh
+++ build-wheels.sh
@@ -5,10 +5,10 @@ export HOME=/tmp
 python setup.py sdist
 # Wheels will be written to /tmp before being auditwheel-repaired
 cd /tmp
-python -m pip install -r "${build_dir}/dev-requirements.txt"
-python -m pip wheel --no-deps --wheel-dir /tmp "${build_dir}"/dist/*.tar.gz
+pip install -r "${build_dir}/dev-requirements.txt"
+pip wheel --no-deps --wheel-dir /tmp "${build_dir}"/dist/*.tar.gz
 ls ./*.whl | xargs -n1 --verbose auditwheel repair --wheel-dir "${build_dir}/dist"
 # Install packages and test
-python -m pip install pygohcl --no-index -f "${build_dir}/dist"
+pip install pygohcl --no-index -f "${build_dir}/dist"
 # Auditwheel-repaired wheels
 ls -al "${build_dir}/dist"
